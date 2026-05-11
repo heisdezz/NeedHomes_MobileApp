42efa71b9cd9605c901caaed202aac6e18e8d869
destiny <desto4q@gmail.com>
2026-05-11 22:39:15 +0100

    messages

--- app/investor/(main)/(tabs)/message.tsx
+++ app/investor/(main)/(tabs)/message.tsx
@@ -1,13 +1,12 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "@/lib/tw";
+import ChatPage from "@/components/CHAT/ChatPage";
 
 export default function MessageScreen() {
   return (
-    <SafeAreaView style={tw`flex-1 bg-bg-light`}>
-      <View style={tw`flex-1 items-center justify-center`}>
-        <Text style={tw`text-white/50 text-base`}>Messages</Text>
-      </View>
+    <SafeAreaView style={tw`flex-1 bg-bg-light`} edges={["top", "bottom"]}>
+      <ChatPage />
     </SafeAreaView>
   );
 }
