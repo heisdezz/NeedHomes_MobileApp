b1d655bb1b92bc7bca4386756fd927f058e51ac6
heisdezz <desto4q@gmail.com>
2026-05-19 15:51:48 +0100

    updat4es

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
-const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require('expo/metro-config');
+// const config = getDefaultConfig(__dirname);
+// config.server = {
+//   ...config.server,
+//   host: '0.0.0.0',
+// };
+// module.exports = config;
 
+const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
-
-config.resolver.unstable_enablePackageExports = true;
-
+config.server = {
+  ...config.server,
+  host: "0.0.0.0",
+};
 module.exports = config;
